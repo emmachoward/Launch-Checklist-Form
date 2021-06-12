05562be0e303ce805b9f530cd0c0a64ae3670b3e
Emma Howard <emmachoward@gmail.com>
2021-06-11 20:57:13 -0500

    Added basic form validation

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         alert("All fields are required!");
+      }
+      if (!isNaN(pilotName.value) || !isNaN(copilotName.value) || isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+         alert("Aake sure to enter valid information for each field!")
+      }
+   });
+});
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
