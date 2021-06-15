0f1848a2fbc199b528fe0497e057d8d912b23f6b
Emma Howard <emmachoward@gmail.com>
2021-06-14 21:38:04 -0500

    added JSON plantary data

--- script.js
+++ script.js
@@ -40,6 +40,20 @@ window.addEventListener("load", function() {
          launchStatus.innerHTML = "Shuttle is ready for launch";
          launchStatus.style.color = "green";
          document.getElementById("faultyItems").style.visibility = "hidden";
+         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+            response.json().then(function(json){
+               let missionTarget = document.getElementById("missionTarget");
+               missionTarget.innerHTML = `<h2>Mission Destination</h2>
+               <ol>
+                  <li>Name: ${json[1].name}</li>
+                  <li>Diameter: ${json[1].diameter}</li>
+                  <li>Star: ${json[1].star}</li>
+                  <li>Distance from Earth: ${json[1].distance}</li>
+                  <li>Number of Moons: ${json[1].moons}</li>
+               </ol>
+               <img src="${json[1].image}"></img>`
+            });
+         }); 
       }
 
 
